392f7c84150742472d7dd0734bd92e4099b367cb
phghuy <phghuy.it@gmail.com>
2026-03-08 23:14:11 +0700

    fix bug

--- resources/views/layouts/frontend/pages/home.blade.php
+++ resources/views/layouts/frontend/pages/home.blade.php
@@ -7,7 +7,7 @@
         <div class="my-12">
             <p class="capitalize text-3xl font-bold mb-6 ml-3">Sách Bán Chạy</p>
 
-            <div class="grid grid-cols-4 gap-10 mx-6">
+            <div class="grid grid-cols-2 xl:grid-cols-4 gap-10 mx-6">
             <x-product-card title="Nhà Giả Kim" price="151000" author="Paulo Coelho" year=2020 img="https://m.media-amazon.com/images/I/51TjK1b8uaL.AC_SX250.jpg"/>
             <x-product-card title="Nhà Giả Kim" price="151000" author="Paulo Coelho" year=2020 img="https://m.media-amazon.com/images/I/51ISNDCCBAL.AC_SX250.jpg"/>
             <x-product-card title="Nhà Giả Kim" price="151000" author="Paulo Coelho" year=2020/>
@@ -17,7 +17,7 @@
         <div class="my-12">
             <p class="capitalize text-3xl font-bold mb-6 ml-3">Sách Giảm Giá Sâu</p>
 
-            <div class="grid grid-cols-4 gap-10 mx-6">
+            <div class="grid grid-cols-2 xl:grid-cols-4 gap-10 mx-6">
             <x-product-card title="Nhà Giả Kim" price="151000" author="Paulo Coelho" year=2020/>
             <x-product-card title="Nhà Giả Kim" price="151000" author="Paulo Coelho" year=2020/>
             <x-product-card title="Nhà Giả Kim" price="151000" author="Paulo Coelho" year=2020/>
@@ -27,7 +27,7 @@
         <div class="my-12">
             <p class="capitalize text-3xl font-bold mb-6 ml-3">sách Vừa ra lò</p>
 
-            <div class="grid grid-cols-4 gap-10 mx-6">
+            <div class="grid grid-cols-2 xl:grid-cols-4 gap-10 mx-6">
             <x-product-card title="Nhà Giả Kim" price="151000" author="Paulo Coelho" year=2020/>
             <x-product-card title="Nhà Giả Kim" price="151000" author="Paulo Coelho" year=2020/>
             <x-product-card title="Nhà Giả Kim" price="151000" author="Paulo Coelho" year=2020/>
@@ -37,7 +37,7 @@
         <div class="my-12">
             <p class="capitalize text-3xl font-bold mb-6 ml-3">Sách kinh điển</p>
 
-            <div class="grid grid-cols-4 gap-10 mx-6">
+            <div class="grid grid-cols-2 xl:grid-cols-4 gap-10 mx-6">
             <x-product-card title="Nhà Giả Kim" price="151000" author="Paulo Coelho" year=2020/>
             <x-product-card title="Nhà Giả Kim" price="151000" author="Paulo Coelho" year=2020/>
             <x-product-card title="Nhà Giả Kim" price="151000" author="Paulo Coelho" year=2020/>
--- resources/views/layouts/frontend/pages/products/index.blade.php
+++ resources/views/layouts/frontend/pages/products/index.blade.php
@@ -5,7 +5,7 @@
         <div class="my-12">
             <p class="capitalize text-3xl font-bold mb-6 ml-3">Tất cả sản phẩm</p>
 
-            <div class="grid grid-cols-4 gap-10 mx-6">
+            <div class="grid grid-cols-2 xl:grid-cols-4 gap-10 mx-6">
             <x-product-card title="Nhà Giả Kim" price="151000" author="Paulo Coelho" year=2020 img="https://m.media-amazon.com/images/I/51TjK1b8uaL.AC_SX250.jpg"/>
             <x-product-card title="Nhà Giả Kim" price="151000" author="Paulo Coelho" year=2020 img="https://m.media-amazon.com/images/I/51ISNDCCBAL.AC_SX250.jpg"/>
             <x-product-card title="Nhà Giả Kim" price="151000" author="Paulo Coelho" year=2020 img="https://m.media-amazon.com/images/I/51TjK1b8uaL.AC_SX250.jpg"/>
