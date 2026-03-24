895e9de88958eea51f4a1497a9354e4425dcbd81
phghuy <phghuy.it@gmail.com>
2026-03-25 06:39:10 +0700

    query db in frontend with jquery

--- app/Http/Controllers/frontend/ProductController.php
+++ app/Http/Controllers/frontend/ProductController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers\frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
     public function index(){
-        return view('layouts.frontend.pages.products.index');
+        $query=Product::query();
+        $query->with(['category:id,name','brand:id,name'])
+        ->select('id','image','name','price_buy','category_id','brand_id','status')
+        ->orderBy('created_at','desc')
+        ->where('status',1)
+        ->get();
+        $products=$query->paginate(8);
+        return view('layouts.frontend.pages.products.index',compact('products'));
     }
     
     public function detail(){
--- resources/views/layouts/frontend/pages/products/index.blade.php
+++ resources/views/layouts/frontend/pages/products/index.blade.php
@@ -2,19 +2,48 @@
 <x-slot:title>
     Sản Phẩm
 </x-slot:title>
-    <div class="text-[#0f1111] mt-6 w-[80%] mx-auto">
-        <div class="my-12">
-            <p class="capitalize text-3xl font-bold mb-6 ml-3">Tất cả sản phẩm</p>
-
-            <div class="grid grid-cols-2 xl:grid-cols-4 gap-10 mx-6">
-            <x-product-card title="Nhà Giả Kim" price="151000" author="Paulo Coelho" year=2020 img="https://m.media-amazon.com/images/I/51TjK1b8uaL.AC_SX250.jpg"/>
-            <x-product-card title="Nhà Giả Kim" price="151000" author="Paulo Coelho" year=2020 img="https://m.media-amazon.com/images/I/51ISNDCCBAL.AC_SX250.jpg"/>
-            <x-product-card title="Nhà Giả Kim" price="151000" author="Paulo Coelho" year=2020 img="https://m.media-amazon.com/images/I/51TjK1b8uaL.AC_SX250.jpg"/>
-            <x-product-card title="Nhà Giả Kim" price="151000" author="Paulo Coelho" year=2020 img="https://m.media-amazon.com/images/I/51ISNDCCBAL.AC_SX250.jpg"/>
-            <x-product-card title="Nhà Giả Kim" price="151000" author="Paulo Coelho" year=2020 img="https://m.media-amazon.com/images/I/51TjK1b8uaL.AC_SX250.jpg"/>
-            <x-product-card title="Nhà Giả Kim" price="151000" author="Paulo Coelho" year=2020 img="https://m.media-amazon.com/images/I/51ISNDCCBAL.AC_SX250.jpg"/>
-            <x-product-card title="Nhà Giả Kim" price="151000" author="Paulo Coelho" year=2020 img="https://m.media-amazon.com/images/I/51TjK1b8uaL.AC_SX250.jpg"/>
-            <x-product-card title="Nhà Giả Kim" price="151000" author="Paulo Coelho" year=2020 img="https://m.media-amazon.com/images/I/51ISNDCCBAL.AC_SX250.jpg"/>
+<x-slot:header>
+    <script src="{{asset('js/jquery-4.0.0.min.js')}}"></script>
+</x-slot:header>
+    <div class="flex">
+        <div class="flex-4">
+            <p class="capitalize text-3xl font-bold m-6">Tất cả sản phẩm</p>
+            <div class="text-[#0f1111] mt-6 w-[80%] mx-auto">
+                <div class="my-12 grid grid-cols-2 xl:grid-cols-4 gap-3">
+                    @foreach ($products as $product)
+                        <x-ui.productcard :product="$product"></x-ui.productcard>
+                    @endforeach
+                </div>
+                <div class="mt-4 ">
+                    {{ $products->links() }}
+                </div>
+            </div>
         </div>
+        <aside class="flex-1 border-l border-black p-3">
+            <p class="m-6 capitalize ">Sap xep<i class="fa-solid fa-filter"></i></p>
+            <p>The Loai Sach</p>
+            <ul>
+                <li><input type="radio" name="categoru_id" value="1">Van hoc</li>
+                <li><input type="radio" name="categoru_id" value="2">Fantasy</li>
+                <li><input type="radio" name="categoru_id" value="6">Tam ly hoc</li>
+            </ul>
+            <p>Khoang gia</p>
+            <ul>
+                <li><input type="checkbox" name="price_range" data-min=0 data-max=200>0-200k</li>
+                <li><input type="checkbox" name="price_range" data-min=200 data-max=1000>200k-1tr</li>
+                <li><input type="checkbox" name="price_range" data-min=1000 >1tr+</li>
+            </ul>
+            <p>Tac Gia</p>
+            <ul>
+                <li><input type="checkbox" name="brand_id" value="1">J.K. Rowling</li>
+                <li><input type="checkbox" name="brand_id" value="2">George Orwell</li>
+                <li><input type="checkbox" name="brand_id" value="3">J.R.R. Tolkien</li>
+            </ul>
+        </aside>
     </div>
+    <x-slot:footer>
+        <script>
+            
+        </script>
+    </x-slot:footer>
 </x-frontend.layout>
